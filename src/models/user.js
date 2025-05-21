2adc1ceeb8a0128f3f8da3a0ed5d6227e2dd7ba4
Uday246r <udaychauhan246r@gamil.com>
2025-05-21 16:28:45 +0530

    final_evaluation_done_IP-2(user_email_validation)

--- src/models/user.js
+++ src/models/user.js
@@ -15,6 +15,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
+        lowercase:true,
         trim: true,
         validate(value){
             if(!validator.isEmail(value)){
